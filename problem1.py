5d5500f884d52a9b36cbef42361e282e1fa08b78
ivan <ivanko@ivan.ru>
2020-10-31 12:53:04 +0300

    лабораторная 6

--- problem1.py
+++ problem1.py
@@ -1,46 +1,11 @@
-Ivan1 = int(input('Рост Бори = '))
-Ivan2 = int(input('Рост Димы = '))
-Ivan3 = int(input('Рост Вовы = '))
-
-max_val = 0
-mid_val = 0
-low_val = 0
-
-if Ivan1 > Ivan2:
-    if Ivan1 > Ivan3:
-        max_val = Ivan1
-        if Ivan2 > Ivan3:
-            mid_val = Ivan2
-            low_val = Ivan3
-        else:
-            mid_val = Ivan3
-            low_val = Ivan2
-    elif Ivan3 > Ivan2:
-        max_val = Ivan3
-        mid_val = Ivan1
-        low_val = Ivan2
-else:
-    if Ivan2 > Ivan3:
-        max_val = Ivan2
-        if Ivan3 > Ivan1:
-            mid_val = Ivan3
-            low_val = Ivan1
-        else:
-            mid_val = Ivan3
-            low_val = Ivan1
-    else:
-        max_val = Ivan3
-        if Ivan1 > Ivan2:
-            mid_val = Ivan1
-            low_val = Ivan2
-        else:
-            mid_val = Ivan2
-            low_val = Ivan1
-
-
-print(max_val)
-print(mid_val)
-print(low_val)
-
-
-
+s = input('Введите число в двоичной системе - ')
+cou = 0
+for i in str(s):
+    if i == "1":
+        cou = cou + 1
+count = 0
+for i in str(s):
+    if i == "0":
+        count = count + 1
+print('Нулей -',count)
+print('Едениц -',cou)
--- problem2.py
+++ problem2.py
@@ -1,3 +1,18 @@
-s = input('Введите слово - ')
-print('Слова',s,'Имеет длину (всего букв)',len(s)),
+n = str(input("Введите число в двоичной системе - "))
+is_binary = True#Изначально
+for a in n:#Цикл
+    if a.isdigit():#True если (а) полностью число если да то True если нет то False
+        if a in ["1", "0"]:#True если в нём число 1 или 0 если да то True если нет то False
+            continue
+        else:
+            is_binary = False
+            break
+    else:
+        is_binary = False
+        break
+if is_binary:#True
+    print('Я потратил на это 4 часа и это работает')
+else:#False
+    print('Пожалуйста введите число в ДВОИЧНОЙ СИСТЕМЕ')
+
 
